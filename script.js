fc4f6470bcc09d962edfe2dd7bbd2815c64fee9f
ShaiFletcher <shaixperiaz1@gmail.com>
2021-01-24 18:03:31 +0200

    finish list task with comments

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-//add three var to js
+//three var 
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
+//create a responsive button 
+button.onclick = function() {
+let myItem = input.value;
+//value a string
+input.value = '';
+//create element for each item
+const listItem = document.createElement('li');  
+const listText = document.createElement('span');
+const listBtn = document.createElement('button');
+//append span and button as children of list item
+listItem.appendChild(listText);
+listText.textContent = myItem;
+listItem.appendChild(listBtn);
+//create delete option
+listBtn.textContent = 'Delete';
+list.appendChild(listItem);
+//when clicked, remove child ftom list
+listBtn.onclick = function(e) {
+list.removeChild(listItem);
+}
+//focus the input element for entering the next shopping list item
+input.focus();
+}
